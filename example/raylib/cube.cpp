918c596a5798e6ff859225860717a40212272762
Bensuperpc <bensuperpc@gmail.com>
2022-12-30 14:39:50 +0100

    Improve cube
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- example/raylib/cube.cpp
+++ example/raylib/cube.cpp
@@ -13,22 +13,6 @@
 #include "raylib-cpp.hpp"
 #include "raylib.h"
 
-/*
-void generate(std::vector<unsigned char>& v, int width, int height, int depth, siv::PerlinNoise& perlin)
-{
-#pragma omp parallel for collapse(3) schedule(auto)
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            for (int z = 0; z < depth; z++) {
-                const uint8_t value_int =
-                        static_cast<uint8_t>(perlin.octave3D_01(x / 100.0, y / 100.0, z / 100.0, 16, 0.1) * 255.0);
-                v[x * height * depth + y * depth + z] = value_int;
-            }
-        }
-    }
-}
-*/
-
 struct cube
 {
     float x;
@@ -38,18 +22,62 @@ struct cube
     float size_y;
     float size_z;
     Color color;
+    bool display;
 };
 
-
-void generate(std::vector<unsigned char>& v, std::vector<cube>& c, int width, int height, siv::PerlinNoise& perlin)
+void generate(
+    std::vector<unsigned char>& v, std::vector<cube>& cubes, int width, int height, int depth, siv::PerlinNoise& perlin)
 {
+#pragma omp parallel for collapse(3) schedule(auto)
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      for (int z = 0; z < depth; z++) {
+        const uint8_t value_int =
+            static_cast<uint8_t>(perlin.octave3D_01(x / 128.0, y / 128.0, z / 128.0, 16, 0.1) * 255.0);
+        v[x * height * depth + y * depth + z] = value_int;
+      }
+    }
+  }
+
+  // cout max and min
+  auto minmax = std::minmax_element(v.begin(), v.end());
+  std::cout << "min: " << static_cast<int>(*minmax.first) << std::endl;
+  std::cout << "max: " << static_cast<int>(*minmax.second) << std::endl;
+
+  const float cube_size = 1.0f;
+
 #pragma omp parallel for collapse(2) schedule(auto)
   for (int x = 0; x < width; x++) {
     for (int y = 0; y < height; y++) {
-      const uint8_t value_int = static_cast<uint8_t>(perlin.octave2D_01(x / 100.0, y / 100.0, 16, 0.1) * 255.0);
-      v[x * height + y] = value_int;
+      size_t vec_index = y * width + x;
+      unsigned char noise_value = v[vec_index] / 16;
+      for (int z = 0; z < depth; z++) {
+        if (z < noise_value) {
+          cubes[vec_index * depth + z] = cube {x * cube_size,
+                                               z * cube_size,
+                                               y * cube_size,
+                                               cube_size,
+                                               cube_size,
+                                               cube_size,
+                                               raylib::Color::Gray(),
+                                               true};
+        } else {
+          cubes[vec_index * depth + z] = cube {x * cube_size,
+                                               z * cube_size,
+                                               y * cube_size,
+                                               cube_size,
+                                               cube_size,
+                                               cube_size,
+                                               raylib::Color::Gray(),
+                                               false};
+        }
+      }
     }
   }
+
+  auto visible = [](const cube& c) { return c.display; };
+  auto count = std::count_if(cubes.begin(), cubes.end(), visible);
+  std::cout << "Only " << count << " cubes are visible out of " << cubes.size() << std::endl;
 }
 
 int main()
@@ -59,34 +87,28 @@ int main()
 
   const float cube_size = 1.0f;
 
-  raylib::Window window(screen_width, screenHeight, "raylib [core] example - 3d camera first person");
+  raylib::Window window(screen_width, screenHeight, "Minecube");
 
-  siv::PerlinNoise::seed_type seed = 12345u;
+  siv::PerlinNoise::seed_type seed = 2647393077u;
   std::cout << "seed: " << seed << std::endl;
 
   siv::PerlinNoise perlin {seed};
 
-  size_t vecX = 64;
-  size_t vecY = 64;
-  /*
-  size_t vecZ = 1;
-  std::vector<unsigned char> v(vecX * vecY * vecZ, 0);
-  generate(v, vecX, vecY, vecZ, perlin);
-  */
-  std::vector<unsigned char> v(vecX * vecY, 0);
-  std::vector<cube> c(vecX * vecY, cube{0, 0, 0, 0, 0, 0, Color{0, 0, 0, 0}});
-  generate(v, c, vecX, vecY, perlin);
+  size_t vecX = 128;
+  size_t vecY = 128;
+  size_t vecZ = 128;
 
-  // cout max and min
-  auto minmax = std::minmax_element(v.begin(), v.end());
-  std::cout << "min: " << static_cast<int>(*minmax.first) << std::endl;
-  std::cout << "max: " << static_cast<int>(*minmax.second) << std::endl;
+  size_t vec_size = vecX * vecY * vecZ;
+  std::vector<unsigned char> v(vec_size, 0);
+  std::vector<cube> cubes(vec_size, cube {0, 0, 0, 0, 0, 0, Color {0, 0, 0, 0}, false});
+  generate(v, cubes, vecX, vecY, vecZ, perlin);
 
-  raylib::Camera camera(raylib::Vector3(static_cast<float>(vecX * cube_size / 4), 24.0f, static_cast<float>(vecX * cube_size/ 4)),
-                        raylib::Vector3(static_cast<float>(vecX * cube_size / 2), 0.0f, static_cast<float>(vecX * cube_size / 2)),
-                        raylib::Vector3(0.0f, 1.0f, 0.0f),
-                        60.0f,
-                        CAMERA_PERSPECTIVE);
+  raylib::Camera camera(
+      raylib::Vector3(static_cast<float>(vecX * cube_size / 4), 64.0f, static_cast<float>(vecX * cube_size / 4)),
+      raylib::Vector3(static_cast<float>(vecX * cube_size / 2), 0.0f, static_cast<float>(vecX * cube_size / 2)),
+      raylib::Vector3(0.0f, 1.0f, 0.0f),
+      60.0f,
+      CAMERA_PERSPECTIVE);
 
   camera.SetMode(CAMERA_ORBITAL);
 
@@ -99,11 +121,7 @@ int main()
       decltype(seed) seed = std::random_device()();
       std::cout << "seed: " << seed << std::endl;
       perlin.reseed(seed);
-      generate(v, c, vecX, vecY, perlin);
-
-      auto minmax = std::minmax_element(v.begin(), v.end());
-      std::cout << "min: " << static_cast<int>(*minmax.first) << std::endl;
-      std::cout << "max: " << static_cast<int>(*minmax.second) << std::endl;
+      generate(v, cubes, vecX, vecY, vecZ, perlin);
     }
 
     BeginDrawing();
@@ -111,26 +129,24 @@ int main()
       window.ClearBackground(RAYWHITE);
 
       camera.BeginMode();
-
-      for (int y = 0; y < vecY; y++) {
-        for (int x = 0; x < vecX; x++) {
-          // Noise value is equal to the height of the column at this position
-          // 0 = lowest, no cube
-          // 255 = highest, 255 cubes high
-          size_t vec_index = y * vecX + x;
-          unsigned char noise_value = v[vec_index] / 16;
-
-
-          for (int z = 0; z < noise_value; z++) {
-            DrawCube(
-                Vector3(x * cube_size, z * cube_size, y * cube_size), cube_size, cube_size, cube_size, raylib::Color::Gray());
-            DrawCubeWires(Vector3(x * cube_size, z * cube_size, y * cube_size),
-                          cube_size,
-                          cube_size,
-                          cube_size,
-                          raylib::Color::DarkGray());
-          }
+#pragma omp parallel for schedule(auto) num_threads(1)
+      for (size_t i = 0; i < cubes.size(); i++) {
+        if (!cubes[i].display) {
+          continue;
         }
+
+        // Draw only surfaces
+        if (cubes[i + 1].display) {
+          continue;
+        }
+#pragma omp critical
+        DrawCubeV(raylib::Vector3(cubes[i].x, cubes[i].y, cubes[i].z),
+                  raylib::Vector3(cubes[i].size_x, cubes[i].size_y, cubes[i].size_z),
+                  cubes[i].color);
+#pragma omp critical
+        DrawCubeWiresV(raylib::Vector3(cubes[i].x, cubes[i].y, cubes[i].z),
+                       raylib::Vector3(cubes[i].size_x, cubes[i].size_y, cubes[i].size_z),
+                       raylib::Color::Black());
       }
 
       camera.EndMode();
